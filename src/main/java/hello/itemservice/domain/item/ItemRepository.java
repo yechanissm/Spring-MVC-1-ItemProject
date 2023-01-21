dbb8e18d355897bcb4f708e8cc6ac70c731021a3
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-21 21:05:35 +0900

    상품 도메인 개발- 핵심 비지니스 로직

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,41 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(),item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+        //한번 감싸는 이유는 변경되어도 실제 store에는 변화가 없게 안전하게 하기 위해서이다.
+    }
+
+    public void update(Long itemid, Item updateParam){
+        Item findItem = findById(itemid);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
